c474f178bcd616b26d251ffae96b973faded41a2
Gerrit Elenbaas <84500222+Gelenhaus@users.noreply.github.com>
2022-01-04 13:25:26 -0700

    needed to get rid of old variables

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -31,13 +31,13 @@ const SavedBooks = () => {
         variables: { bookId }
       });
       removeBookId(bookId);
-    } catch (errr) {
+    } catch (err) {
       console.error(err);
     }
 
   }
   // if data isn't here yet, say so
-  if (!userDataLength) {
+  if (!loading) {
     return <h2>LOADING...</h2>;
   }
 
