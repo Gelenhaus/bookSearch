4c581092c6e8ffa6f29c62d31a94f2e0d856eedd
Gerrit Elenbaas <84500222+Gelenhaus@users.noreply.github.com>
2022-01-05 15:45:44 -0700

    I forgot to update the middleware function in auth.js

--- server/server.js
+++ server/server.js
@@ -20,13 +20,15 @@ const startServer = async () => {
   });
 
   await server.start();
+
   server.applyMiddleware({ app });
+
   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 };
 
 startServer();
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
@@ -34,9 +36,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
